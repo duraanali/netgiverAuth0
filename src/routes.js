5379a7f714f0348991234e945449dac6249d4f6d
Duraan Ali <ali@sahalsolutions.com>
2019-11-12 22:08:43 -0600

    another message2

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,18 @@
 import React from 'react'
-import { Route, IndexRedirect } from 'react-router'
-import AuthService from './utils/AuthService'
-import Container from './views/Main/Container'
-import Home from './views/Main/Home/Home'
-import Login from './views/Main/Login/Login'
+import { Route } from 'react-router'
 
-const auth = new AuthService('kW0w0ej0qpY2pWPU0BXeDV6BK8fVBvfP', 'dev-2lmqt1n7.auth0.com');
+import makeMainRoutes from './views/Main/routes'
 
-// onEnter callback to validate authentication in private routes
-const requireAuth = (nextState, replace) => {
-  if (!auth.loggedIn()) {
-    replace({ pathname: '/login' })
-  }
-}
+export const makeRoutes = () => {
+  const main = makeMainRoutes();
 
-export const makeMainRoutes = () => {
   return (
-    <Route path="/" component={Container} auth={auth}>
-      <IndexRedirect to="/home" />
-      <Route path="home" component={Home} onEnter={requireAuth} />
-      <Route path="login" component={Login} />
-      <Route path="access_token=:token" component={Login} /> //to prevent router errors
+    <Route path=''>
+      {main}
     </Route>
   )
 }
 
-export default makeMainRoutes
+
+
+export default makeRoutes
--- src/views/Main/Login/Login.js
+++ src/views/Main/Login/Login.js
@@ -12,32 +12,13 @@ export class Login extends React.Component {
     location: T.object,
     auth: T.instanceOf(AuthService)
   }
-  // We’ll add a constructor that checks if the user is already logged in or not and redirects to the home page when a user is logged in.
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      profile: props.auth.getProfile()
-    }
-    props.auth.on('profile_updated', (newProfile) => {
-      this.setState({ profile: newProfile })
-      this.context.router.push('/home');
-    })
-  }
 
   render() {
     const { auth } = this.props
-    if (this.state.profile.user_id) {
-      this.context.router.push('/home');
-    }
-    // Where in the seed project, there is only one login method, here we implement a login button for each of the three passwordless login methods we are going to be working with.
     return (
       <div className={styles.root}>
-        <h2>Login</h2>
-        <ButtonToolbar className={styles.toolbar}>
-          <Button bsStyle="primary" onClick={auth.loginMagiclink.bind(this)}>Login with Magiclink</Button>
-          <Button bsStyle="primary" onClick={auth.loginEmailCode.bind(this)}>Login with Email Code</Button>
-          <Button bsStyle="primary" onClick={auth.loginSMSCode.bind(this)}>Login with SMS Code</Button>
-        </ButtonToolbar>
+        <h2>Experience MFA like a Boss!!!</h2>
+        <Button bsStyle="primary" onClick={auth.login.bind(this)}>Login to Proceed >></Button>
       </div>
     )
   }
